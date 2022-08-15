fdc7d8e4cdfc1d0d3b0784a11d3702897dbb02aa
JoshMcDaniel <jrmcdaniel08@yahoo.com>
2022-08-14 23:07:37 -0400

    Centered text alignment on Intro section

--- components/shared/SectionIntroImage.tsx
+++ components/shared/SectionIntroImage.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Typography } from '@mui/material';
+import { Box, Typography, useMediaQuery, useTheme } from '@mui/material';
 import FixedBackgroundImage from './FixedBackgroundImage';
 
 type SectionIntroImageProps = {
@@ -8,11 +8,7 @@ type SectionIntroImageProps = {
 };
 
 export const SectionIntroImage = (props: SectionIntroImageProps) => {
-  const centerStyle = {
-    display: 'grid',
-    justifyContent: 'center',
-  };
-  const height = '50vh';
+  const isMediumView = useMediaQuery(useTheme().breakpoints.up('md'));
 
   return (
     <Box
@@ -23,48 +19,27 @@ export const SectionIntroImage = (props: SectionIntroImageProps) => {
         justifyContent: 'center',
         alignContent: 'center',
         color: '#FFFFFF',
+        height: '50vh',
+        textAlign: 'center',
       }}
-      height={height}
     >
       <FixedBackgroundImage
         imgPath={props.imagePath}
         height="100%"
         width="100%"
       />
-      <Container>
-        <Typography
-          variant="h4"
-          component="h1"
-          display={{ xs: 'block', md: 'none' }}
-        >
+      <Box
+        sx={{
+          maxWidth: '90vw',
+        }}
+      >
+        <Typography variant={isMediumView ? 'h3' : 'h4'} component="h1">
           {props.primaryText}
         </Typography>
-        <Typography
-          variant="h3"
-          component="h1"
-          display={{ xs: 'none', md: 'block' }}
-        >
-          {props.primaryText}
-        </Typography>
-      </Container>
-      <Container style={centerStyle}>
-        <Typography
-          variant="subtitle1"
-          component="p"
-          textAlign="center"
-          display={{ xs: 'block', md: 'none' }}
-        >
+        <Typography variant={isMediumView ? 'h5' : 'subtitle1'} component="p">
           {props.subText}
         </Typography>
-        <Typography
-          variant="h5"
-          component="p"
-          textAlign="center"
-          display={{ xs: 'none', md: 'block' }}
-        >
-          {props.subText}
-        </Typography>
-      </Container>
+      </Box>
     </Box>
   );
 };
